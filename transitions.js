2ce387aba9ce6858f7838c449a4d079ef5990406
br4h14m <160452565+br4h14m@users.noreply.github.com>
2024-11-23 22:36:53 -0500

    Add files via upload

---
+++ transitions.js
@@ -0,0 +1,4 @@
+// Función para añadir la clase de transición al cuerpo al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.classList.add("fade-in");
+});
